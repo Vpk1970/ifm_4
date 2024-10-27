180787cadc21e1b2a2c0bc957e0a5584b1400ff9
Ville-Petteri Koivunen <vp.koivunen@hotmail.fi>
2024-10-27 14:55:27 +0200

    Add files via upload

---
+++ AlienLuontiTehdas_2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienLuontiTehdas_2 : MonoBehaviour
+{
+    public GameObject Alien_5;
+    public float offset = 1.5f; // alienien väli joka suunnassa
+
+    //private float aikaajaljella = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*for (float y = 1f; y <= 4f; y = y + 1f)
+        {
+            for (float x = 1f; x <= 8f; x = x + 1f)
+            {
+                GameObject apualien = Instantiate(Alien_5, new Vector3(x - 8 * offset + 2, 5 - y * offset, 0f), Quaternion.identity);
+            }   
+        }*/
+
+        Invoke(nameof(AlienLuonti_2), 3.0f);
+        //AlienLuonti_2();
+
+        
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public GameObject AlienLuonti_2()
+    {
+        /*for (int i = 0;i < 1000000000; i++)
+        {
+
+        }*/
+        //Debug.Log("ajastin loppu");
+
+        //if (aikaajaljella < 0)
+        //{
+            for (float y = 0f; y <= 3f; y = y + 1f)
+            {
+                for (float x = 0f; x <= 7f; x = x + 1f)
+                {
+                GameObject gameObject1 = Instantiate(Alien_5, new Vector3(-8 + (offset * x), 4 - (offset * y), 0f), Quaternion.identity);
+                }
+            }
+            //aikaajaljella = 10;
+
+        //}
+        // aikaajaljella -= Time.deltaTime;
+
+
+        return gameObject;
+
+        //Alien_5.name = "alien";
+
+    }
+}
+/* Pseudoa:
+ * 
+ * Begin
+ *  offset = 1.5
+ *      for y = 1 to 4
+ *          for x = 1 to 8
+ *              Luo alien_5(-8 + (offset * x), 4 - (offset * y), 0)
+ *          endfor
+ *      endfor
+ * Endbegin
+ */
+
---
+++ Aluksen_luonti_2.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aluksen_luonti : MonoBehaviour
+{
+    public GameObject alus = null;
+    public int elossa = 1;
+    public Ajastin aika;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //aika = gameObject.AddComponent<Ajastin>();// Ajastin luokka on koodia gameobject issa
+        //aika.aseta_aika(3f);// sekunnit       
+        Invoke("luo_alus", 1.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(elossa == 1 && aika.ajastin_toiminnassa == false)
+        {
+            
+
+        }*/
+
+        
+    }
+    public void luo_alus()
+    {
+        GameObject apualus = Instantiate(this.alus, new Vector3(0f, -4f, 0f), Quaternion.identity);
+        elossa = 0;
+    }
+}
